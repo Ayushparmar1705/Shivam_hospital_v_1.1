5eb2827bdbf79fbae4e40b1d47306b6899337b1a
ayush170504 <ayush.techupr@outlook.com>
2026-05-19 22:50:31 +0530

    change the server.js code and add the vercel.json file

--- backend/server.js
+++ backend/server.js
@@ -82,15 +82,15 @@
 // app.listen(PORT, () => {
 //   console.log(`Backend server running on port ${PORT}`);
 // });
-import axios from "axios";
-
 export default async function handler(req, res) {
   try {
     const url = `https://serpapi.com/search.json?engine=google_maps_reviews&data_id=0x3958dc870a151ff9:0xef999dd84c53a16c&hl=en&sort_by=ratingHigh&api_key=${process.env.SERPAPI_KEY}`;
 
-    const response = await axios.get(url);
+    const response = await fetch(url);
 
-    const allReviews = response.data?.reviews || [];
+    const data = await response.json();
+
+    const allReviews = data?.reviews || [];
 
     const positive = allReviews.filter(
       (review) => review.rating >= 4
@@ -101,7 +101,7 @@ export default async function handler(req, res) {
     res.status(200).json(selected);
 
   } catch (error) {
-    console.error(error);
+    console.log(error);
 
     res.status(500).json({
       error: "Failed to fetch reviews"
