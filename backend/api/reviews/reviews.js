a3f0cda4a748e76ec01dc0729f8b5e250cdf0df4
ayush170504 <ayush.techupr@outlook.com>
2026-05-20 21:33:42 +0530

    change the code for reviews.js

--- backend/api/reviews/reviews.js
+++ backend/api/reviews/reviews.js
@@ -1,5 +1,23 @@
-const reviews = require("../data/reviews.json");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = (req, res) => {
-  res.status(200).json(reviews);
+  try {
+    const filePath = path.join(
+      process.cwd(),
+      "data",
+      "reviews.json"
+    );
+
+    const jsonData = fs.readFileSync(filePath, "utf8");
+
+    const reviews = JSON.parse(jsonData);
+
+    res.status(200).json(reviews);
+  } catch (error) {
+    res.status(500).json({
+      error: "Failed to load reviews",
+      message: error.message
+    });
+  }
 };
